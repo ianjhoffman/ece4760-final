c5f5d43ca7ed9bc2f71d193dd3d84a989e3aab9d
Ian Hoffman <ijh6@cornell.edu>
2016-11-17 20:15:12 -0500

    envelope keyframes

---
+++ Code/env_keyframes.h
@@ -0,0 +1,41 @@
+#ifdef ENV_KEYFRAMES
+#define ENV_KEYFRAMES
+
+// ENVELOPE KEYFRAMES:
+//    - 1ms rise, 2ms fall
+//    - 1ms rise, 100ms fall
+//    - 1ms rise, 800ms fall
+//    - 100ms rise, 667ms fall
+//    - 400ms rise, 400ms fall
+//    - 667ms rise, 100ms fall
+//    - 800ms rise, 1ms fall
+//    - 100ms rise, 1ms fall
+//    - 2ms rise, 1ms fall
+
+const unsigned int atk_incs[] = {
+    42949673,
+    42949673,
+    42949673,
+    429497,
+    85899,
+    64425,
+    53687,
+    429497,
+    21474836
+};
+
+const unsigned int decay_incs[] = {
+    21474836,
+    429497,
+    53687,
+    64425,
+    85899,
+    429497,
+    42949673,
+    42949673,
+    42949673
+};
+
+const unsigned int num_keyframes = 9;
+
+#endif
---
+++ env_bound_accum_calcs.rb
@@ -0,0 +1,34 @@
+ISR_FREQUENCY = 100000 # 100 kHz DDS sample frequency
+INCREMENTS_TO_OVERFLOW = 4294967296 # 2^32 = value at which envelope maxes/mins out
+
+# keyframe points in modal envelope transitions ("frequency" of envelope rise/fall)
+envelope_keyframes =
+[
+	[1000, 500], # 1ms rise, 2ms fall
+	[1000, 10], # 1ms rise, 100ms fall
+	[1000, 1.25], # 1ms rise, 800ms fall
+	[10, 1.5], # 100ms rise, 667ms fall
+	[2, 2], # 400ms rise, 400ms fall
+	[1.5, 10], # 667ms rise, 100ms fall
+	[1.25, 1000], # 800ms rise, 1ms fall
+	[10, 1000], # 100ms rise, 1ms fall
+	[500, 1000], # 2ms rise, 1ms fall
+]
+
+# freq = 1 / ( ( INCREMENTS_TO_OVERFLOW / PHASE_INCREMENT ) * ( 1 / ISR_FREQUENCY ) )
+# 1 / freq = ( INCREMENTS_TO_OVERFLOW * ( 1 / ISR_FREQUENCY ) ) / PHASE_INCREMENT
+# PHASE_INCREMENT = INCREMENTS_TO_OVERFLOW * ( 1 / ISR_FREQUENCY ) * freq
+# round it to the nearest int because we store these as ints
+
+env_increments = envelope_keyframes.map {|x| x.map {|y| ( INCREMENTS_TO_OVERFLOW * (1.0/ISR_FREQUENCY) * y).round } }
+
+# print envelope keyframes in C header format
+puts "const unsigned int atk_incs[] = {"
+env_increments.each_with_index {|x, idx| puts "    #{x[0]}#{(idx != 8) ? "," : ""}"}
+puts "};"
+
+puts "const unsigned int decay_incs[] = {"
+env_increments.each_with_index {|x, idx| puts "    #{x[1]}#{(idx != 8) ? "," : ""}"}
+puts "};"
+
+puts "const unsigned int num_keyframes = 9;"
